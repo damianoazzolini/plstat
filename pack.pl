a0b63c8def492d0c59f1999992964e14995c1be3
Damiano Azzolini <damiano.azzolini@unife.it>
2021-05-15 15:41:43 +0200

    fix naming

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(plstat).
-title('plstats - Statistics with Prolog').
+title('plstat - Statistics with Prolog').
 version('1.0').
 author('Damiano Azzolini', 'damiazz94@gmail.com').
 home('https://github.com/damianoazzolini/plstat').
